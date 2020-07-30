1f1112a103b25622e5b4417ad4ffdfa70341cdc5
Francesco <zuppif@usi.ch>
2020-07-30 14:20:31 +0200

    a

--- joke/Joke.py
+++ joke/Joke.py
@@ -4,7 +4,7 @@ class Joke:
     """
     def tell(self, n: int = 1):
         """
-        Tell a random joke
+        Tell a random joke ahhaha!
 
         Args:
             n (int, optional): Number of jokes. Defaults to 1.
